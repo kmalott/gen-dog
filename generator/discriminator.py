00e80191f04e43a09e1e13d612650a7020ef1081
kmalott <kmalott2@gmail.com>
2025-03-25 15:12:22 -0600

    add gan discriminator

---
+++ generator/discriminator.py
@@ -0,0 +1,16 @@
+import torch
+
+class Discriminator(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        layers = []
+        layers.append(torch.nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1))
+        layers.append(torch.nn.LeakyReLU(0.2))
+        layers.append(torch.nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1))
+        layers.append(torch.nn.LeakyReLU(0.2))
+        layers.append(torch.nn.Conv2d(128, 1, kernel_size=4, stride=2, padding=1))
+        layers.append(torch.nn.AdaptiveAvgPool2d(1))
+        self.model = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
