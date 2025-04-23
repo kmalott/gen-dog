8e87b19e43b826d4384f01164e10024e1fed3e88
kmalott <kmalott2@gmail.com>
2025-04-23 09:00:00 -0600

    masked attention

--- generator/train_masked.py
+++ generator/train_masked.py
@@ -20,10 +20,10 @@ def train(exp_dir: str = "logs",
     batch_size: int = 1024,
     seed: int = 2024,
     codebook: int = 14,
-    latent: int = 1024,
-    d_model: int = 512,
-    nhead: int = 8,
-    nlayer: int = 4,
+    latent: int = 512,
+    d_model: int = 256,
+    nhead: int = 2,
+    nlayer: int = 2,
     **kwargs,
 ):
     if torch.cuda.is_available():
@@ -43,17 +43,17 @@ def train(exp_dir: str = "logs",
     masked.to(device)
     # load data loaders
     train_token = TokenDataset("train")
-    train_token = torch.utils.data.Subset(train_token, indices=list(range(0,100)))
+    train_token = torch.utils.data.Subset(train_token, indices=list(range(0,1)))
     val_token = TokenDataset("val")
     train_data = torch.utils.data.DataLoader(train_token, batch_size=batch_size, num_workers=4, shuffle=True)
     val_data = torch.utils.data.DataLoader(val_token, batch_size=batch_size, num_workers=4, shuffle=False)
     # create optimizer
     optimizer = torch.optim.AdamW(params=masked.parameters(), lr=lr)
-    scheduler = transformers.get_cosine_schedule_with_warmup(
-        optimizer, 
-        num_warmup_steps=len(train_data)*3,
-        num_training_steps=len(train_data)*num_epoch
-    )
+    # scheduler = transformers.get_cosine_schedule_with_warmup(
+    #     optimizer, 
+    #     num_warmup_steps=len(train_data)*3,
+    #     num_training_steps=len(train_data)*num_epoch
+    # )
 
     global_step = 0
     # training loop
@@ -71,21 +71,26 @@ def train(exp_dir: str = "logs",
             x = x.squeeze(1).to(device)
             x = x.flatten(start_dim=1)
             B, S = x.shape
-            mask = torch.rand((B, 1024), device=device) < torch.rand(1, device=device)
+            ratio = torch.tensor([0.1])
+            # ratio = torch.rand(1, device=device)
+            # while ratio > 0.5:
+            #     ratio = torch.rand(1, device=device)
+            mask = torch.rand((B, S)) < ratio
+            total = mask.sum()
+            attn_mask = torch.where(mask, float('-inf'), 0.0).repeat(S, 1)
             target = x.clone()
-            logits = masked(x, mask)
+            logits = masked(x, attn_mask)
             loss = F.cross_entropy(logits[mask], target[mask])
             acc = (torch.sum(logits[mask].argmax(dim=1) == target[mask])).cpu()
-            total = target[mask].shape[0]
-            train_acc += acc
-            train_total += total
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            scheduler.step()
+            # scheduler.step()
             train_loss += loss.item()
+            train_acc += acc
+            train_total += total
             global_step += 1
-        # train_loss /= train_total
+        train_loss /= train_total
         train_acc /= train_total
 
         # disable gradient computation and switch to evaluation mode
@@ -95,12 +100,17 @@ def train(exp_dir: str = "logs",
         #         x = x.squeeze(1).to(device)
         #         x = x.flatten(start_dim=1)
         #         B, S = x.shape
-        #         mask = torch.rand((B, 1024), device=device) < torch.rand(1, device=device)
+        #         ratio = torch.tensor([0.1])
+        #             # ratio = torch.rand(1, device=device)
+        #             # while ratio > 0.5:
+        #             #     ratio = torch.rand(1, device=device)
+        #         mask = torch.rand((B, S)) < ratio
+        #         total = mask.sum()
+        #         attn_mask = torch.where(mask, float('-inf'), 0.0).repeat(S, 1)
         #         target = x.clone()
-        #         logits = masked(x, mask)
+        #         logits = masked(x, attn_mask)
         #         loss = F.cross_entropy(logits[mask], target[mask])
         #         acc = (torch.sum(logits[mask].argmax(dim=1) == target[mask])).cpu()
-        #         total = target[mask].shape[0]
         #         val_acc += acc
         #         val_total += total
         #         val_loss += loss.item()
@@ -115,7 +125,7 @@ def train(exp_dir: str = "logs",
             f"Epoch {epoch + 1:2d} / {num_epoch:2d}: \n"
             f"train_loss={train_loss} \n"
             f"val_loss={val_loss} \n"
-            f"train_acc={train_acc}"
+            f"train_acc={train_acc} \n" 
             f"val_acc={val_acc}"
         )
 
@@ -132,10 +142,10 @@ if __name__ == "__main__":
     parser.add_argument("--seed", type=int, default=2024)
     parser.add_argument("--batch_size", type=int, default=1024)
     parser.add_argument("--codebook", type=int, default=14)
-    parser.add_argument("--latent", type=int, default=1024)
-    parser.add_argument("--d_model", type=int, default=512)
-    parser.add_argument("--nhead", type=int, default=8)
-    parser.add_argument("--nlayer", type=int, default=4)
+    parser.add_argument("--latent", type=int, default=512)
+    parser.add_argument("--d_model", type=int, default=256)
+    parser.add_argument("--nhead", type=int, default=2)
+    parser.add_argument("--nlayer", type=int, default=2)
 
     # pass all arguments to train
     train(**vars(parser.parse_args()))
